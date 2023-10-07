317da6a12ba46d1f45871e94587b46657a38ebc6
Susan <sulpengel@gmail.com>
2023-10-07 16:36:33 -0300

    3 aulas

---
+++ dicionario.py
@@ -0,0 +1,54 @@
+## Dicionário = conjunto não-ordenado de pares de CHAVE ; onde as chaves são únicas em uma dada instância
+# do dicionário. Dicionário são delimitados por chaves {} e contem uma lista de pares chave: valor separada por vírgulas. 
+
+#Declarando dicionários
+pessoa = {"nome": "Guilherme", "idade": 28}
+print(pessoa)
+
+pessoa = dict(nome="Guilherme", idade=28)
+print(pessoa)
+
+pessoa["telefone"] = "3333-1234"  # {"nome": "Guilherme", "idade": 28, "telefone": "3333-1234"}
+print(pessoa)
+
+# Acessando os dados
+dados = {"nome": "Guilherme", "idade": 28, "telefone": "3333-1234"}
+
+print(dados["nome"])  # "Guilherme"
+print(dados["idade"])  # 28
+print(dados["telefone"])  # "3333-1234"
+
+dados["nome"] = "Maria"
+dados["idade"] = 18
+dados["telefone"] = "9988-1781"
+
+print(dados)  # {"nome": "Maria", "idade": 18, "telefone": "9988-1781"}
+
+#Dicionários Aninhados
+contatos = {
+    "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"},
+    "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
+    "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
+    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
+}
+
+telefone = contatos["giovanna@gmail.com"]["telefone"]  # "3443-2121"
+print(telefone)
+
+#Interando dicionários
+contatos = {
+    "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"},
+    "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
+    "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
+    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
+}
+
+for chave in contatos:
+    print(chave, contatos[chave])
+
+print("=" * 100)
+
+for chave, valor in contatos.items():
+    print(chave, valor)
+    
+
---
+++ dicionarios_Metodos.py
@@ -0,0 +1,143 @@
+contatos = {
+    "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"},
+    "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
+    "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
+    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
+}
+
+contatos.clear()
+print(contatos)  # {}
+
+## COPY
+
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+copia = contatos.copy()
+copia["guilherme@gmail.com"] = {"nome": "Gui"}
+
+print(contatos["guilherme@gmail.com"])  # {"nome": "Guilherme", "telefone": "3333-2221"}
+
+print(copia["guilherme@gmail.com"])  # {"nome": "Gui"}
+
+## FROMKEYS
+
+resultado = dict.fromkeys(["nome", "telefone"])  # {"nome": None, "telefone": None}
+print(resultado)
+
+resultado = dict.fromkeys(["nome", "telefone"], "vazio")  # {"nome": "vazio", "telefone": "vazio"}
+print(resultado)
+
+## COPY
+
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+# contatos["chave"]  # KeyError
+
+resultado = contatos.get("chave")  # None
+print(resultado)
+
+resultado = contatos.get("chave", {})  # {}
+print(resultado)
+
+resultado = contatos.get(
+    "guilherme@gmail.com", {}
+)  # {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}
+print(resultado)
+
+## ITENS
+
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+resultado = contatos.items()  # dict_items([('guilherme@gmail.com', {'nome': 'Guilherme', 'telefone': '3333-2221'})])
+print(resultado)
+
+## KEYS
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+resultado = contatos.keys()  # dict_keys(['guilherme@gmail.com'])
+print(resultado)
+
+# POP
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+resultado = contatos.pop("guilherme@gmail.com")  # {'nome': 'Guilherme', 'telefone': '3333-2221'}
+print(resultado)
+
+resultado = contatos.pop("guilherme@gmail.com", {})  # {}
+print(resultado)
+
+# POPITEM
+
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+resultado = contatos.popitem()  # ('guilherme@gmail.com', {'nome': 'Guilherme', 'telefone': '3333-2221'})
+print(resultado)
+
+# contatos.popitem()  # KeyError
+
+## SET DEFAULT
+contato = {"nome": "Guilherme", "telefone": "3333-2221"}
+
+contato.setdefault("nome", "Giovanna")  # "Guilherme"
+print(contato)  # {'nome': 'Guilherme', 'telefone': '3333-2221'}
+
+contato.setdefault("idade", 28)  # 28
+print(contato)  # {'nome': 'Guilherme', 'telefone': '3333-2221', 'idade': 28}
+
+## UPDATE
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+contatos.update({"guilherme@gmail.com": {"nome": "Gui"}})
+print(contatos)  # {'guilherme@gmail.com': {'nome': 'Gui'}}
+
+contatos.update({"giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3322-8181"}})
+# {'guilherme@gmail.com': {'nome': 'Gui'}, 'giovanna@gmail.com': {'nome': 'Giovanna', 'telefone': '3322-8181'}}
+print(contatos)
+
+## VALUES
+contatos = {
+    "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"},
+    "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
+    "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
+    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
+}
+
+resultado = (
+    contatos.values()
+)  # dict_values([{'nome': 'Guilherme', 'telefone': '3333-2221'}, {'nome': 'Giovanna', 'telefone': '3443-2121'}, {'nome': 'Chappie', 'telefone': '3344-9871'}, {'nome': 'Melaine', 'telefone': '3333-7766'}])  # noqa
+print(resultado)
+
+## IN
+contatos = {
+    "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"},
+    "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
+    "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
+    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
+}
+
+resultado = "guilherme@gmail.com" in contatos  # True
+print(resultado)
+
+resultado = "megui@gmail.com" in contatos  # False
+print(resultado)
+
+resultado = "idade" in contatos["guilherme@gmail.com"]  # False
+print(resultado)
+
+resultado = "telefone" in contatos["giovanna@gmail.com"]  # True
+print(resultado)
+
+#DEL
+
+contatos = {
+    "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"},
+    "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
+    "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
+    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
+}
+
+del contatos["guilherme@gmail.com"]["telefone"]
+del contatos["chappie@gmail.com"]
+
+# {'guilherme@gmail.com': {'nome': 'Guilherme'}, 'giovanna@gmail.com': {'nome': 'Giovanna', 'telefone': '3443-2121'}, 'melaine@gmail.com': {'nome': 'Melaine', 'telefone': '3333-7766'}}  # noqa
+print(contatos)
